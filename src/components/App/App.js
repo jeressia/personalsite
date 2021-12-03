9fec6d2f4a933b5c8c48fbd4f04d9ba60026aa9e
jeressia <jeressiajaytv@gmail.com>
2021-12-02 22:02:42 -0600

    initial setup

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Navbar</h2>
+      <h1>Header</h1>
+      <h2>Woman</h2>
+      <h2>Wisdom</h2>
+      <h2>Work</h2>
+      <h2>Wins</h2>
+    </div>
+  );
+}
+
+export default App;
